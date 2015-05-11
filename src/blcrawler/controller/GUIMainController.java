f260054e3179a7186930a21219a0a09a0d10d20d
Joe Gallagher <joedgallagher@yahoo.com>
2015-05-11 00:59:57 -0500

    Enter listener, writes verification to eclipse console

--- src/blcrawler/controller/GUIMainController.java
+++ src/blcrawler/controller/GUIMainController.java
@@ -11,12 +11,55 @@ public class GUIMainController
 {
 	
 	private static GUIModel gui;
+	Action Thing;
 	
 	public GUIMainController(GUIModel gui) 
 	{
 		this.gui = gui;
+		
+
+		Thing = new AbstractAction() 
+		{
+			@Override
+			public void actionPerformed(ActionEvent e) 
+			{
+				System.out.println("Hello World");
+			}
+		};
 	}
 	
+	/**
+	 * @return the gui
+	 */
+	public static GUIModel getGui() 
+	{
+		return gui;
+	}
+
+	/**
+	 * @param gui the gui to set
+	 */
+	public static void setGui(GUIModel gui) 
+	{
+		GUIMainController.gui = gui;
+	}
+
+	/**
+	 * @return the thing
+	 */
+	public Action getThing() 
+	{
+		return Thing;
+	}
+
+	/**
+	 * @param thing the thing to set
+	 */
+	public void setThing(Action thing) 
+	{
+		Thing = thing;
+	}
+
 	public void buildGUI() 
 	{
 
--- src/blcrawler/view/GUIView.java
+++ src/blcrawler/view/GUIView.java
@@ -5,15 +5,20 @@ package blcrawler.view;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.KeyStroke;
 
 import net.miginfocom.swing.MigLayout;
 
@@ -34,6 +39,8 @@ public class GUIView {
 	private JTextArea consoleOut;
 	private JTextArea commandLine;
 	private JScrollPane scrollableConsole;
+	private static final String key = "ENTER";
+    private KeyStroke keyStroke;
 	
 
 	public GUIView(GUIMainController guiMainController, GUIModel guiModel) 
@@ -60,10 +67,15 @@ public class GUIView {
 		  consoleOut.setEditable(true);
 		  consoleOut.setLineWrap(true);
 		  JScrollPane scrollableConsole = new JScrollPane(consoleOut);
+		  
+		  
 				  
 		commandLine = new JTextArea(5,45);
+		keyStroke = KeyStroke.getKeyStroke(key);
 		  commandLine.setEditable(true);
 		  commandLine.setLineWrap(true);
+		  commandLine.getInputMap(JComponent.WHEN_FOCUSED).put(keyStroke,"Thing");
+		  commandLine.getActionMap().put("Thing", this.guiMainController.getThing());
 
 	      headerLabel = new JLabel(guiModel.getHeaderLabel(),JLabel.CENTER );
 	      statusLabel = new JLabel(guiModel.getStatusLabel(),JLabel.CENTER);        
